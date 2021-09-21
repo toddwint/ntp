f6ca19fa9cc86ba624abddfd8daca9fc2bfbde91
toddwint <todd.wintermute@gmail.com>
2021-09-21 17:11:50 -0500

    Cleaned up comments; renamed some files

--- build/scripts/startup.sh
+++ build/scripts/startup.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-##
-## example environment variables can be found in the .env file
-##
-##
-
 ## Run the commands to make it all work
 ln -fs /usr/share/zoneinfo/$TZ /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
