f8c9b7da99ae2269aba39199db292b160549cab9
toddwint <todd.wintermute@gmail.com>
2021-09-28 12:04:15 -0500

    updated startup.sh commands

--- build/scripts/startup.sh
+++ build/scripts/startup.sh
@@ -10,6 +10,8 @@ sed -ri "s|^#(statsdir /var/log/ntpstats/)|\1|" /etc/ntp.conf
 sed -ri 's/^(statistics loopstats peerstats clockstats)/\1 rawstats sysstats/' /etc/ntp.conf
 sed -ri '/filegen clockstats file clockstats type day enable/a filegen sysstats file sysstats type day enable' /etc/ntp.conf
 sed -ri '/filegen clockstats file clockstats type day enable/a filegen rawstats file rawstats type day enable' /etc/ntp.conf
+sed -ri '/# Use servers from the NTP Pool Project/,/pool ntp.ubuntu.com/d' /etc/ntp.conf
+sed -ri '/# Specify one or more NTP servers./a server 127.127.1.0 prefer' /etc/ntp.conf
 
 service ntp stop
 service ntp start
