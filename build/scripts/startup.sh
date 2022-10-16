96861621699531d7e57ccfa4542462274d4a9ae9
toddwint <todd.wintermute@gmail.com>
2022-10-15 22:30:06 -0500

    added some polish

--- build/scripts/startup.sh
+++ build/scripts/startup.sh
@@ -10,12 +10,6 @@ echo $HOSTNAME > /etc/hostname
 gunzip /usr/local/bin/frontail.gz
 gunzip /usr/local/bin/tailon.gz
 
-# Create logs folder and init files
-mkdir -p /opt/"$APPNAME"/logs/ntpstats
-touch /opt/"$APPNAME"/logs/"$APPNAME".log
-truncate -s 0 /opt/"$APPNAME"/logs/"$APPNAME".log
-echo "$(date -Is) [Start of $APPNAME log file]" >> /opt/"$APPNAME"/logs/"$APPNAME".log
-
 # Disable rsyslog kernel logs and start rsyslogd
 sed -Ei '/imklog/s/^[^#]/#/' /etc/rsyslog.conf
 sed -Ei '/immark/s/^#//' /etc/rsyslog.conf
@@ -32,6 +26,23 @@ service ntp stop
 service ntp start
 service ntp status
 
+# Link the log to the app log
+mkdir -p /opt/"$APPNAME"/logs/ntpstats
+ln -s /var/log/syslog /opt/"$APPNAME"/logs/"$APPNAME".log
+ln -s /var/log/ntpstats/rawstats /opt/"$APPNAME"/logs/ntpstats/rawstats
+ln -s /var/log/ntpstats/peerstats /opt/"$APPNAME"/logs/ntpstats/peerstats
+ln -s /var/log/ntpstats/loopstats /opt/"$APPNAME"/logs/ntpstats/loopstats
+ln -s /var/log/ntpstats/clockstats /opt/"$APPNAME"/logs/ntpstats/clockstats
+ln -s /var/log/ntpstats/sysstats /opt/"$APPNAME"/logs/ntpstats/sysstats
+#ln /var/log/remote/syslog.log /opt/"$APPNAME"/logs/"$APPNAME".log
+
+# Create logs folder and init files
+mkdir -p /opt/"$APPNAME"/logs/ntpstats
+touch /opt/"$APPNAME"/logs/"$APPNAME".log
+truncate -s 0 /opt/"$APPNAME"/logs/"$APPNAME".log
+#echo "$(date -Is) [Start of $APPNAME log file]" >> /opt/"$APPNAME"/logs/"$APPNAME".log
+logger "$(date -Is) [Start of $APPNAME log file]"
+
 # Start web interface
 NLINES=1000
 cp /opt/"$APPNAME"/scripts/tmux.conf /root/.tmux.conf
@@ -45,10 +56,10 @@ sed -Ei 's/tail -n 500/tail -n '"$NLINES"'/' /opt/"$APPNAME"/scripts/tmux.sh
 nohup ttyd -p "$HTTPPORT2" -t titleFixed="${APPNAME}|${APPNAME}.log" -t fontSize=18 -t 'theme={"foreground":"black","background":"white", "selection":"red"}' /opt/"$APPNAME"/scripts/tmux.sh >> /opt/"$APPNAME"/logs/ttyd2.log 2>&1 &
 # ttyd tmux without color
 #nohup ttyd -p "$HTTPPORT2" -t titleFixed="${APPNAME}|${APPNAME}.log" -T xterm-mono -t fontSize=18 -t 'theme={"foreground":"black","background":"white", "selection":"red"}' /opt/"$APPNAME"/scripts/tmux.sh >> /opt/"$APPNAME"/logs/ttyd2.log 2>&1 &
-nohup frontail -n "$NLINES" -p "$HTTPPORT3" /var/log/syslog /var/log/ntpstats/* >> /opt/"$APPNAME"/logs/frontail.log 2>&1 &
+nohup frontail -n "$NLINES" -p "$HTTPPORT3" /opt/"$APPNAME"/logs/"$APPNAME".log /opt/"$APPNAME"/logs/ntpstats/* >> /opt/"$APPNAME"/logs/frontail.log 2>&1 &
 sed -Ei 's/\$lines/'"$NLINES"'/' /opt/"$APPNAME"/scripts/tailon.toml
 sed -Ei '/^listen-addr = /c listen-addr = [":'"$HTTPPORT4"'"]' /opt/"$APPNAME"/scripts/tailon.toml
-nohup tailon -c /opt/"$APPNAME"/scripts/tailon.toml /var/log/syslog /var/log/ntpstats/rawstats /var/log/ntpstats/peerstats /var/log/ntpstats/loopstats /var/log/ntpstats/clockstats /var/log/ntpstats/sysstats /opt/"$APPNAME"/logs/ttyd1.log /opt/"$APPNAME"/logs/ttyd2.log /opt/"$APPNAME"/logs/frontail.log /opt/"$APPNAME"/logs/tailon.log >> /opt/"$APPNAME"/logs/tailon.log 2>&1 &
+nohup tailon -c /opt/"$APPNAME"/scripts/tailon.toml /opt/"$APPNAME"/logs/"$APPNAME".log /opt/"$APPNAME"/logs/ntpstats/rawstats /opt/"$APPNAME"/logs/ntpstats/peerstats /opt/"$APPNAME"/logs/ntpstats/loopstats /opt/"$APPNAME"/logs/ntpstats/clockstats /opt/"$APPNAME"/logs/ntpstats/sysstats /opt/"$APPNAME"/logs/ttyd1.log /opt/"$APPNAME"/logs/ttyd2.log /opt/"$APPNAME"/logs/frontail.log /opt/"$APPNAME"/logs/tailon.log >> /opt/"$APPNAME"/logs/tailon.log 2>&1 &
 
 # Keep docker running
 bash
--- build/scripts/tmux.sh
+++ build/scripts/tmux.sh
@@ -6,16 +6,16 @@ tmux split-window -v -p 80
 tmux split-window -v -p 80
 tmux split-window -v -p 80
 tmux split-window -v -p 80
-tmux send-keys -t 1 "tail -n 500 -f /var/log/ntpstats/rawstats" Enter
-tmux send-keys -t 2 "tail -n 500 -f /var/log/ntpstats/peerstats" Enter
-tmux send-keys -t 3 "tail -n 500 -f /var/log/ntpstats/loopstats" Enter
-tmux send-keys -t 4 "tail -n 500 -f /var/log/ntpstats/clockstats" Enter
-tmux send-keys -t 5 "tail -n 500 -f /var/log/ntpstats/sysstats" Enter
+tmux send-keys -t 1 "tail -n 500 -f /opt/"$APPNAME"/logs/ntpstats/rawstats" Enter
+tmux send-keys -t 2 "tail -n 500 -f /opt/"$APPNAME"/logs/ntpstats/peerstats" Enter
+tmux send-keys -t 3 "tail -n 500 -f /opt/"$APPNAME"/logs/ntpstats/loopstats" Enter
+tmux send-keys -t 4 "tail -n 500 -f /opt/"$APPNAME"/logs/ntpstats/clockstats" Enter
+tmux send-keys -t 5 "tail -n 500 -f /opt/"$APPNAME"/logs/ntpstats/sysstats" Enter
 #tmux selectp -t 1 -d #disable user input in pane
 #tmux selectp -t 1 -e #enable user input in pane
 tmux select-layout even-vertical
 tmux new-window
-tmux send-keys -t 1 "tail -n 500 -f /var/log/syslog" Enter
+tmux send-keys -t 1 "tail -n 500 -f /opt/"$APPNAME"/logs/"$APPNAME".log" Enter
 echo '
   Welcome to the '"$APPNAME"' docker image.
 
@@ -27,6 +27,7 @@ echo '
   Ctrl + b then Esc         | Exit copy/scroll mode
   Ctrl + b then q then 0-9  | Focus on pane number
   Ctrl + b then x           | Close current pane
+  Ctrl + b then d           | Detach from tmux session
   Ctrl + b then c           | Create a new window
   Ctrl + b then n           | Move to the next window
   Ctrl + b then p           | Move to the previous window
